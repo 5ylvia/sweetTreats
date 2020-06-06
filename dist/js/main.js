8dc8a4d1f2b5468bdac4d7e09feb62cedbe00676
Sylvia <subin3311@gmail.com>
2020-06-06 22:31:01 +1200

    Use case 1 nearly done, need to add JSON

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,34 @@
-console.log("ping");
-var body = $('body');
+$(function() {
+    var videoArray = [{
+        videoId: "BtLwoNJ6klE",
+        title: "Arduino Basics 101",
+        category: "arduino"
+        },
+        {
+        videoId: "YT3birSKLLU",
+        title: "Arduino Basics 101",
+        category: "arduino"
+        }
+    ];
+
+    function makeVideoHTML(videoObject) {
+        return `
+        <div class="video">
+            <img src="https://img.youtube.com/vi/${videoObject.videoId}/mqdefault.jpg" class="video__thumbnail" alt=${videoObject.title}">
+            <h3 class="video__title">${videoObject.title}</h3>
+            <p class="video__category">${videoObject.category}</p>
+        </div>`
+    }
+
+    var html = '';
+    console.log(html);
+
+    for (var i = 0; i < videoArray.length; i++) {
+        html += makeVideoHTML(videoArray[i]);
+        console.log(html);
+
+    }
+
+    $('#video-thumbnails').append(html);
+
+});
